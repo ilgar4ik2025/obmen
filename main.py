c845604a1f07828026e7faf5220e6854b0de10f7
ilgar4ik2025 <pmdpro@yandex.ru>
2025-04-04 14:00:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('TELEGRAM_TOKEN')
-PORT = int(os.getenv('PORT', 8443))
-WEBHOOK_URL = os.getenv('WEBHOOK_URL')  # Убедитесь, что имя переменной WEBHOOK_URL
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -19,12 +17,8 @@ def main():
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     
-    # Обновляем настройки webhook в соответствии с переменными Railway
-    application.run_webhook(
-        listen="0.0.0.0",
-        port=PORT,
-        webhook_url=WEBHOOK_URL
-    )
+    # Используем polling вместо webhook
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
